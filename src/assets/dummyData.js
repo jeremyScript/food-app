008d3d45e89ad347763a709213e7f906eb2d9750
Jeremy Lee <ultrdy@gmail.com>
2021-08-31 16:10:26 -0500

    Add dummy data

---
+++ src/assets/dummyData.js
@@ -0,0 +1,28 @@
+const DUMMY_FOOD = [
+  {
+    id: "f1",
+    name: "Sushi",
+    description: "Finest fish and veggies",
+    price: 22.99,
+  },
+  {
+    id: "f2",
+    name: "Schnitzel",
+    description: "A german specialty!",
+    price: 16.5,
+  },
+  {
+    id: "f3",
+    name: "Barbecue Burger",
+    description: "American, raw, meaty",
+    price: 12.99,
+  },
+  {
+    id: "f4",
+    name: "Green Bowl",
+    description: "Healthy...and green...",
+    price: 18.99,
+  },
+];
+
+export default DUMMY_FOOD;
