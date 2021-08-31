4a5b915eed75f346f87a9789c2aa3e0a0e8877de
Jeremy Lee <ultrdy@gmail.com>
2021-08-31 16:12:08 -0500

    Edit FoodSummary

--- src/components/Food/FoodSummary.js
+++ src/components/Food/FoodSummary.js
@@ -3,14 +3,14 @@ import styles from "./FoodSummary.module.css";
 const FoodSummary = () => {
   return (
     <section className={styles.summary}>
-      <h2>Delicious Food, Delivered To You</h2>
+      <h2>
+        Delicious Food
+        <br />
+        Delivered to Your Doorstep
+      </h2>
       <p>
         Choose your favorite food from our broad selection and enjoy a delicious
         meal at home.
-      </p>
-      <p>
-        All of our meals are cooked with high-quality ingredients by our
-        experienced chefs!
       </p>
     </section>
   );
