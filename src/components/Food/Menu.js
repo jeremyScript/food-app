6f5e06d3a8a39164a3886abe7203d91cbd00d698
Jeremy Lee <ultrdy@gmail.com>
2021-08-31 17:39:22 -0500

    Integrate FoodItem into Menu

--- src/components/Food/Menu.js
+++ src/components/Food/Menu.js
@@ -2,23 +2,16 @@ import styles from "./Menu.module.css";
 import foodData from "../../assets/dummyData.js";
 
 import Card from "./../UI/Card";
+import FoodItem from "./FoodItem";
 
 const Menu = () => {
-  const foodList = foodData.map((food) => (
-    <li>
-      <div>
-        <span className={styles["food-title"]}>{food.title}</span>
-        <span className={styles["food-description"]}>{food.description}</span>
-        <span className={styles["food-price"]}>{food.price}</span>
-      </div>
-      <div>
-        <div>
-          <label>Amount</label>
-          <input type="number" />
-        </div>
-        <button>+ Add</button>
-      </div>
-    </li>
+  const foodList = foodData.map(({ title, description, price }) => (
+    <FoodItem
+      key={title + price}
+      title={title}
+      description={description}
+      price={price}
+    />
   ));
 
   return (
