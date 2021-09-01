2adb8b0388b631bfcc03f4e23a9fb7a1ae0e6c2d
Jeremy Lee <ultrdy@gmail.com>
2021-09-01 18:31:15 -0500

    Create FoodItemForm

---
+++ src/components/Food/FoodItemForm.js
@@ -0,0 +1,12 @@
+import styles from "./FoodItemForm.module.css";
+
+const FoodItemForm = (props) => {
+  return (
+    <form className={styles.form}>
+      <input />
+      <button>+ Add</button>
+    </form>
+  );
+};
+
+export default FoodItemForm;
