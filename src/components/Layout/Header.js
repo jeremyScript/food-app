28bf183f246e0c28f05a4b46133da0d09bbf3cdb
Jeremy Lee <ultrdy@gmail.com>
2021-09-03 11:06:40 -0500

    Rearrange app structure

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -8,15 +8,17 @@ const Header = (props) => {
   return (
     <>
       <header className={styles.header}>
-        <h1>FoodApp</h1>
-        <div>
-          <HeaderCartButton />
+        <nav className={styles["navbar"]}>
+          <h1>FoodApp</h1>
+          <div>
+            <HeaderCartButton />
+          </div>
+        </nav>
+        <div className={styles["hero-image"]}>
+          <img src={heroImage} alt="A table full of food" />
         </div>
+        <FoodSummary />
       </header>
-      <div className={styles["hero-image"]}>
-        <img src={heroImage} alt="A table full of food" />
-      </div>
-      <FoodSummary />
     </>
   );
 };
