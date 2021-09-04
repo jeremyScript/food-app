da65c78eb7192bf78e7ff06a05b4de3f622bdff0
Jeremy Lee <ultrdy@gmail.com>
2021-09-03 20:35:23 -0500

    Use ref forwarding to read input

--- src/components/Food/FoodItemForm.js
+++ src/components/Food/FoodItemForm.js
@@ -1,13 +1,22 @@
+import { useRef } from "react";
+
 import styles from "./FoodItemForm.module.css";
 
 import Input from "../UI/Input";
 
 const FoodItemForm = (props) => {
+  const quantityInputRef = useRef();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={handleSubmit}>
       <Input
+        ref={quantityInputRef}
         input={{
-          id: "quantity_" + props.id,
+          id: props.id,
           type: "number",
           min: "1",
           max: "99",
