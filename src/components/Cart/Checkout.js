e70013257afe054c2d083be4d8f482d4b717e055
Jeremy Lee <ultrdy@gmail.com>
2021-09-27 18:04:38 -0500

    Update Checkout form

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -4,12 +4,32 @@ const Checkout = (props) => {
   return (
     <form>
       <div className={styles.control}>
-        <label htmlFor=""></label>
-        <input type="text" id="" />
+        <label htmlFor="first-name">First name</label>
+        <input type="text" id="first-name" />
       </div>
       <div className={styles.control}>
-        <label htmlFor=""></label>
-        <input type="text" id="" />
+        <label htmlFor="address-1">Address line 1</label>
+        <input type="text" id="address-1" />
+      </div>
+      <div className={styles.control}>
+        <label htmlFor="postal-code">Postal code</label>
+        <input type="text" id="postal-code" />
+      </div>
+      <div className={styles.control}>
+        <label htmlFor="city">City</label>
+        <input type="text" id="city" />
+      </div>
+      <div className={styles.control}>
+        <label htmlFor="country">Country</label>
+        <input type="text" id="country" />
+      </div>
+      <div className={styles.control}>
+        <label htmlFor="state">State</label>
+        <input type="text" id="state" />
+      </div>
+      <div className={styles.control}>
+        <label htmlFor="phone">Contact phone</label>
+        <input type="text" id="phone" />
       </div>
     </form>
   );
