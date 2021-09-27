fa764029f3a45f4cf84b0f288aaa52e8d5d7435c
Jeremy Lee <ultrdy@gmail.com>
2021-09-27 15:55:17 -0500

    Add initial fetch logic

--- src/components/Food/Menu.js
+++ src/components/Food/Menu.js
@@ -1,11 +1,36 @@
-import styles from "./Menu.module.css";
-import foodData from "../../assets/dummyData.js";
+import { useEffect, useState } from "react";
 
+import styles from "./Menu.module.css";
 import Card from "./../UI/Card";
 import FoodItem from "./FoodItem";
 
 const Menu = () => {
-  const foodList = foodData.map(({ id, name, description, price }) => (
+  const [menuData, setMenuData] = useState([]);
+  const [didErrorOccur, setDidErrorOccur] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const fetchMenuData = async () => {
+    try {
+      const response = await fetch(
+        "https://food-app-6dd5d-default-rtdb.firebaseio.com/menu"
+      );
+
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+
+      const data = await response.json();
+      setMenuData(data);
+    } catch (error) {
+      setDidErrorOccur(true);
+    }
+  };
+
+  useEffect(() => {
+    fetchMenuData();
+  }, []);
+
+  const foodList = menuData.map(({ id, name, description, price }) => (
     <FoodItem
       key={name}
       id={id}
