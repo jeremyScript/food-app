9950cd3358b9130e8da4759a7a11e2d6a7a8ac4b
Jeremy Lee <ultrdy@gmail.com>
2021-09-27 17:58:37 -0500

    Create Checkout

---
+++ src/components/Cart/Checkout.js
@@ -0,0 +1,18 @@
+import styles from "./Checkout.module.css";
+
+const Checkout = (props) => {
+  return (
+    <form>
+      <div className={styles.control}>
+        <label htmlFor=""></label>
+        <input type="text" id="" />
+      </div>
+      <div className={styles.control}>
+        <label htmlFor=""></label>
+        <input type="text" id="" />
+      </div>
+    </form>
+  );
+};
+
+export default Checkout;
