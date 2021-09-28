8ab57e81ddc3f7bf291049eb116b9bf0aee57d05
Jeremy Lee <ultrdy@gmail.com>
2021-09-28 16:12:42 -0500

    Add prelim styles

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -6,34 +6,54 @@ const Checkout = (props) => {
   };
 
   return (
-    <form onSubmit={handleOrderSubmit}>
-      <div className={styles.control}>
-        <label htmlFor="first-name">First name</label>
-        <input type="text" id="first-name" />
+    <form className={styles["delivery-form"]} onSubmit={handleOrderSubmit}>
+      <div className={styles["input-group"]}>
+        <div className={styles.control}>
+          <label htmlFor="first-name">First name</label>
+          <input type="text" id="first-name" />
+        </div>
+        <div className={styles.control}>
+          <label htmlFor="last-name">Last name</label>
+          <input type="text" id="last-name" />
+        </div>
       </div>
-      <div className={styles.control}>
-        <label htmlFor="address-1">Address line 1</label>
-        <input type="text" id="address-1" />
+      <div className={styles["input-group"]}>
+        <div className={styles.control}>
+          <label htmlFor="address-1">Address line 1</label>
+          <input type="text" id="address-1" />
+        </div>
       </div>
-      <div className={styles.control}>
-        <label htmlFor="postal-code">Postal code</label>
-        <input type="text" id="postal-code" />
+      <div className={styles["input-group"]}>
+        <div className={styles.control}>
+          <label htmlFor="address-2">Address line 2</label>
+          <input type="text" id="address-2" />
+        </div>
       </div>
-      <div className={styles.control}>
-        <label htmlFor="city">City</label>
-        <input type="text" id="city" />
+      <div className={styles["input-group"]}>
+        <div className={styles.control}>
+          <label htmlFor="postal-code">Postal code</label>
+          <input type="text" id="postal-code" />
+        </div>
+        <div className={styles.control}>
+          <label htmlFor="city">City</label>
+          <input type="text" id="city" />
+        </div>
       </div>
-      <div className={styles.control}>
-        <label htmlFor="country">Country</label>
-        <input type="text" id="country" />
+      <div className={styles["input-group"]}>
+        <div className={styles.control}>
+          <label htmlFor="country">Country</label>
+          <input type="text" id="country" />
+        </div>
+        <div className={styles.control}>
+          <label htmlFor="state">State</label>
+          <input type="text" id="state" />
+        </div>
       </div>
-      <div className={styles.control}>
-        <label htmlFor="state">State</label>
-        <input type="text" id="state" />
-      </div>
-      <div className={styles.control}>
-        <label htmlFor="phone">Contact phone</label>
-        <input type="text" id="phone" />
+      <div className={styles["input-group"]}>
+        <div className={styles.control}>
+          <label htmlFor="phone">Contact phone</label>
+          <input type="text" id="phone" />
+        </div>
       </div>
       <div className={styles.actions}>
         <button type="button" onClick={props.onHideCart}>
