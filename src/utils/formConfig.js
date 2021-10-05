4ce08f6789441ca02ac8743fdb80a918ce2bc25c
Jeremy Lee <ultrdy@gmail.com>
2021-10-05 18:27:01 -0500

    Add addtional properties

--- src/utils/formConfig.js
+++ src/utils/formConfig.js
@@ -1,97 +1,115 @@
 export const checkoutForm = {
   firstName: {
+    inputName: "firstName",
     props: {
       className: "left",
       label: "First name",
       input: {
         id: "first-name",
         type: "text",
+        value: "",
       },
     },
     validationRules: [],
   },
   lastName: {
+    inputName: "lastName",
     props: {
       className: "right",
       label: "Last name",
       input: {
         id: "last-name",
         type: "text",
+        value: "",
       },
     },
     validationRules: [],
   },
   addressLine1: {
+    inputName: "addressLine1",
     props: {
       label: "Address line 1",
       input: {
         id: "address-line-1",
         type: "text",
+        value: "",
       },
     },
     validationRules: [],
   },
   addressLine2: {
+    inputName: "addressLine2",
     props: {
       label: "Address line 2",
       input: {
         id: "address-line-2",
         type: "text",
+        value: "",
       },
     },
     validationRules: [],
   },
   city: {
+    inputName: "city",
     props: {
       className: "left",
       label: "City",
       input: {
         id: "city",
         type: "text",
+        value: "",
       },
     },
     validationRules: [],
   },
   state: {
+    inputName: "state",
     props: {
       className: "right",
       label: "State",
       input: {
         id: "state",
         type: "text",
+        value: "",
       },
     },
     validationRules: [],
   },
   zipCode: {
+    inputName: "zipCode",
     props: {
       className: "left",
       label: "Zip code",
       input: {
         id: "zip-code",
         type: "text",
+        value: "",
       },
     },
     validationRules: [],
   },
   phone: {
+    inputName: "phone",
     props: {
       className: "left",
       label: "Phone",
       input: {
         id: "phone",
         type: "text",
+        value: "",
       },
     },
     validationRules: [],
   },
   email: {
+    inputName: "email",
     props: {
       className: "right",
       label: "Email",
       input: {
         id: "email",
         type: "text",
+        value: "",
       },
     },
     validationRules: [],
