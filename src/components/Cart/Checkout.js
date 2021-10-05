a10d935207741997e536faa41cf30f7b16b7f312
Jeremy Lee <ultrdy@gmail.com>
2021-10-05 16:28:47 -0500

    Add dynamic population of input fields

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -1,5 +1,6 @@
 import styles from "./Checkout.module.css";
 
+import { checkoutForm } from "../../utils/formConfig";
 import Input from "../UI/Input";
 
 const Checkout = (props) => {
@@ -7,46 +8,21 @@ const Checkout = (props) => {
     event.preventDefault();
   };
 
+  const inputFields = Object.values(checkoutForm).map(({ props }) => (
+    <Input
+      key={props.label}
+      className={`${styles.control} ${styles[props.className]}`}
+      label={props.label}
+      input={{
+        id: props.input.id,
+        type: props.input.type,
+      }}
+    />
+  ));
+
   return (
     <form className={styles["delivery-form"]} onSubmit={handleOrderSubmit}>
-      <Input
-        className={`${styles.control} ${styles.left}`}
-        label="First name"
-        input={{ id: "first-name", type: "text" }}
-      />
-      <Input
-        className={`${styles.control} ${styles.right}`}
-        label="Last name"
-        input={{ id: "last-name", type: "text" }}
-      />
-      <div className={styles.control}>
-        <label htmlFor="address-1">Address line 1</label>
-        <input type="text" id="address-1" />
-      </div>
-      <div className={styles.control}>
-        <label htmlFor="address-2">Address line 2</label>
-        <input type="text" id="address-2" />
-      </div>
-      <div className={`${styles.control} ${styles.left}`}>
-        <label htmlFor="postal-code">Postal code</label>
-        <input type="text" id="postal-code" />
-      </div>
-      <div className={`${styles.control} ${styles.right}`}>
-        <label htmlFor="city">City</label>
-        <input type="text" id="city" />
-      </div>
-      <div className={`${styles.control} ${styles.left}`}>
-        <label htmlFor="country">Country</label>
-        <input type="text" id="country" />
-      </div>
-      <div className={`${styles.control} ${styles.right}`}>
-        <label htmlFor="state">State</label>
-        <input type="text" id="state" />
-      </div>
-      <div className={styles.control}>
-        <label htmlFor="phone">Contact phone</label>
-        <input type="text" id="phone" />
-      </div>
+      {inputFields}
       <div className={styles.actions}>
         <button
           className={styles["button--cancel"]}
