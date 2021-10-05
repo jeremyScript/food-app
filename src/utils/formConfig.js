68ba3416291b5e3ec78961d3f7874c90e62c192b
Jeremy Lee <ultrdy@gmail.com>
2021-10-05 16:27:39 -0500

    Create formConfig

---
+++ src/utils/formConfig.js
@@ -0,0 +1,99 @@
+export const checkoutForm = {
+  firstName: {
+    props: {
+      className: "left",
+      label: "First name",
+      input: {
+        id: "first-name",
+        type: "text",
+      },
+    },
+    validationRules: [],
+  },
+  lastName: {
+    props: {
+      className: "right",
+      label: "Last name",
+      input: {
+        id: "last-name",
+        type: "text",
+      },
+    },
+    validationRules: [],
+  },
+  addressLine1: {
+    props: {
+      label: "Address line 1",
+      input: {
+        id: "address-line-1",
+        type: "text",
+      },
+    },
+    validationRules: [],
+  },
+  addressLine2: {
+    props: {
+      label: "Address line 2",
+      input: {
+        id: "address-line-2",
+        type: "text",
+      },
+    },
+    validationRules: [],
+  },
+  city: {
+    props: {
+      className: "left",
+      label: "City",
+      input: {
+        id: "city",
+        type: "text",
+      },
+    },
+    validationRules: [],
+  },
+  state: {
+    props: {
+      className: "right",
+      label: "State",
+      input: {
+        id: "state",
+        type: "text",
+      },
+    },
+    validationRules: [],
+  },
+  zipCode: {
+    props: {
+      className: "left",
+      label: "Zip code",
+      input: {
+        id: "zip-code",
+        type: "text",
+      },
+    },
+    validationRules: [],
+  },
+  phone: {
+    props: {
+      className: "left",
+      label: "Phone",
+      input: {
+        id: "phone",
+        type: "text",
+      },
+    },
+    validationRules: [],
+  },
+  email: {
+    props: {
+      className: "right",
+      label: "Email",
+      input: {
+        id: "email",
+        type: "text",
+      },
+    },
+    validationRules: [],
+  },
+};
